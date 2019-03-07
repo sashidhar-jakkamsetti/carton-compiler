b67b41cf6f96aed3cc026173f624b2c6ec57f7e0
Yoshimichi Nakatsuka <y.nakatsuka443@gmail.com>
2019-03-06 21:44:29 -0800

    Started implementing MCG

--- carnot/src/main/java/machineCodeGeneration/DLX.java
+++ carnot/src/main/java/machineCodeGeneration/DLX.java
@@ -238,64 +238,64 @@ public class DLX {
 	}
 
 	// Mnemonic-to-Opcode mapping
-	static final String mnemo[] = {
-	"ADD","SUB","MUL","DIV","MOD","CMP","ERR","ERR","OR","AND","BIC","XOR","LSH","ASH","CHK","ERR",
-	"ADDI","SUBI","MULI","DIVI","MODI","CMPI","ERRI","ERRI","ORI","ANDI","BICI","XORI","LSHI","ASHI","CHKI","ERR",
-	"LDW","LDX","POP","ERR","STW","STX","PSH","ERR","BEQ","BNE","BLT","BGE","BLE","BGT","BSR","ERR",
-	"JSR","RET","RDI","WRD","WRH","WRL","ERR","ERR","ERR","ERR","ERR","ERR","ERR","ERR","ERR","ERR",
-	"ERR","ERR","ERR","ERR","ERR","ERR","ERR","ERR","ERR","ERR","ERR","ERR","ERR","ERR","ERR","ERR"};
-	static final int ADD = 0;  
-	static final int SUB = 1;
-	static final int MUL = 2;
-	static final int DIV = 3;
-	static final int MOD = 4;
-	static final int CMP = 5;
-	static final int OR  = 8;
-	static final int AND = 9;
-	static final int BIC = 10;
-	static final int XOR = 11;
-	static final int LSH = 12;
-	static final int ASH = 13;
-	static final int CHK = 14;
+	public static final String mnemo[] = {
+  "ADD","SUB","MUL","DIV","MOD","CMP","ERR","ERR","OR","AND","BIC","XOR","LSH","ASH","CHK","ERR",
+  "ADDI","SUBI","MULI","DIVI","MODI","CMPI","ERRI","ERRI","ORI","ANDI","BICI","XORI","LSHI","ASHI","CHKI","ERR",
+  "LDW","LDX","POP","ERR","STW","STX","PSH","ERR","BEQ","BNE","BLT","BGE","BLE","BGT","BSR","ERR",
+  "JSR","RET","RDI","WRD","WRH","WRL","ERR","ERR","ERR","ERR","ERR","ERR","ERR","ERR","ERR","ERR",
+  "ERR","ERR","ERR","ERR","ERR","ERR","ERR","ERR","ERR","ERR","ERR","ERR","ERR","ERR","ERR","ERR"};
+	public static final int ADD = 0;  
+	public static final int SUB = 1;
+	public static final int MUL = 2;
+	public static final int DIV = 3;
+	public static final int MOD = 4;
+	public static final int CMP = 5;
+	public static final int OR  = 8;
+	public static final int AND = 9;
+	public static final int BIC = 10;
+	public static final int XOR = 11;
+	public static final int LSH = 12;
+	public static final int ASH = 13;
+	public static final int CHK = 14;
 
-	static final int ADDI = 16;
-	static final int SUBI = 17;
-	static final int MULI = 18;
-	static final int DIVI = 19;
-	static final int MODI = 20;
-	static final int CMPI = 21;
-	static final int ORI  = 24;
-	static final int ANDI = 25;
-	static final int BICI = 26;
-	static final int XORI = 27;
-	static final int LSHI = 28;
-	static final int ASHI = 29;
-	static final int CHKI = 30;
+	public static final int ADDI = 16;
+	public static final int SUBI = 17;
+	public static final int MULI = 18;
+	public static final int DIVI = 19;
+	public static final int MODI = 20;
+	public static final int CMPI = 21;
+	public static final int ORI  = 24;
+	public static final int ANDI = 25;
+	public static final int BICI = 26;
+	public static final int XORI = 27;
+	public static final int LSHI = 28;
+	public static final int ASHI = 29;
+	public static final int CHKI = 30;
 
-	static final int LDW = 32;
-	static final int LDX = 33;
-	static final int POP = 34;
-	static final int STW = 36;
-	static final int STX = 37;
-	static final int PSH = 38; 
+	public static final int LDW = 32;
+	public static final int LDX = 33;
+	public static final int POP = 34;
+	public static final int STW = 36;
+	public static final int STX = 37;
+	public static final int PSH = 38; 
 
-	static final int BEQ = 40;
-	static final int BNE = 41;
-	static final int BLT = 42;
-	static final int BGE = 43;
-	static final int BLE = 44;
-	static final int BGT = 45;
-	static final int BSR = 46;
-	static final int JSR = 48;
-	static final int RET = 49;
+	public static final int BEQ = 40;
+	public static final int BNE = 41;
+	public static final int BLT = 42;
+	public static final int BGE = 43;
+	public static final int BLE = 44;
+	public static final int BGT = 45;
+	public static final int BSR = 46;
+	public static final int JSR = 48;
+	public static final int RET = 49;
 
-	static final int RDI = 50;
-	static final int WRD = 51;
-	static final int WRH = 52;
-	static final int WRL = 53;
+	public static final int RDI = 50;
+	public static final int WRD = 51;
+	public static final int WRH = 52;
+	public static final int WRL = 53;
 	
-	static final int ERR = 63; // error opcode which is insertered by loader 
-	                           // after end of program code
+	public static final int ERR = 63; // error opcode which is insertered by loader 
+	                           			  // after end of program code
 
 	static void disassem(int instructionWord) {
 		op = instructionWord >>> 26; // without sign extension
--- carnot/src/main/java/machineCodeGeneration/MachineCodeGenerator.java
+++ carnot/src/main/java/machineCodeGeneration/MachineCodeGenerator.java
@@ -2,11 +2,15 @@ package machineCodeGeneration;
 
 import dataStructures.Instructions.*;
 import dataStructures.Results.*;
+import dataStructures.Operator.OperatorCode;
 import registerAllocation.*;
+import java.util.*;
+import machineCodeGeneration.DLX;
 
 public class MachineCodeGenerator 
 {
     private RegisterAllocator registerAllocator;
+    private static ArrayList<Integer> program;
 
     public MachineCodeGenerator()
     {
@@ -18,8 +22,88 @@ public class MachineCodeGenerator
         
     }
 
-    public void Compute(Instruction instruction)
+    public void compute(Instruction instruction)
     {
+        OperatorCode opcode = instruction.opcode;
+        if(opcode == OperatorCode.add)
+        {
+            if(instruction.operandY instanceof ConstantResult)
+            {
+                DLX.assemble(DLX.ADDI, ((RegisterResult)instruction.operandX).register,
+                                       ((RegisterResult)instruction.operandX).register,
+                                       ((ConstantResult)instruction.operandY).constant);
+            }
+            else
+            {
+                DLX.assemble(DLX.ADD , ((RegisterResult)instruction.operandX).register,
+                                       ((RegisterResult)instruction.operandX).register,
+                                       ((RegisterResult)instruction.operandY).register);
+            }
+        }
+        else if(opcode == OperatorCode.sub)
+        {
+            if(instruction.operandY instanceof ConstantResult)
+            {
+                DLX.assemble(DLX.SUBI, ((RegisterResult)instruction.operandX).register,
+                                       ((RegisterResult)instruction.operandX).register,
+                                       ((ConstantResult)instruction.operandY).constant);
+            }
+            else
+            {
+                DLX.assemble(DLX.SUB , ((RegisterResult)instruction.operandX).register,
+                                       ((RegisterResult)instruction.operandX).register,
+                                       ((RegisterResult)instruction.operandY).register);
+            }
+        }
+        else if(opcode == OperatorCode.mul)
+        {
+            if(instruction.operandY instanceof ConstantResult)
+            {
+                DLX.assemble(DLX.MULI, ((RegisterResult)instruction.operandX).register,
+                                       ((RegisterResult)instruction.operandX).register,
+                                       ((ConstantResult)instruction.operandY).constant);
+            }
+            else
+            {
+                DLX.assemble(DLX.MUL , ((RegisterResult)instruction.operandX).register,
+                                       ((RegisterResult)instruction.operandX).register,
+                                       ((RegisterResult)instruction.operandY).register);
+            }
+        }
+        else if(opcode == OperatorCode.div)
+        {
+            if(instruction.operandY instanceof ConstantResult)
+            {
+                DLX.assemble(DLX.DIVI, ((RegisterResult)instruction.operandX).register,
+                                       ((RegisterResult)instruction.operandX).register,
+                                       ((ConstantResult)instruction.operandY).constant);
+            }
+            else
+            {
+                DLX.assemble(DLX.DIV , ((RegisterResult)instruction.operandX).register,
+                                       ((RegisterResult)instruction.operandX).register,
+                                       ((RegisterResult)instruction.operandY).register);
+            }
+        }
+        else if(opcode == OperatorCode.cmp)
+        {
+            if(instruction.operandY instanceof ConstantResult)
+            {
+                DLX.assemble(DLX.CMPI, ((RegisterResult)instruction.operandX).register,
+                                       ((RegisterResult)instruction.operandX).register,
+                                       ((ConstantResult)instruction.operandY).constant);
+            }
+            else
+            {
+                DLX.assemble(DLX.CMP , ((RegisterResult)instruction.operandX).register,
+                                       ((RegisterResult)instruction.operandX).register,
+                                       ((RegisterResult)instruction.operandY).register);
+            }
+        }
+    }
 
+    public static ArrayList<Integer> getProgram()
+    {
+        return program;
     }
 }
